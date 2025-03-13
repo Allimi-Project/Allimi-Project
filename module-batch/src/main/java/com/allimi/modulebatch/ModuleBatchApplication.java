e04562b8d2c9c66d38d58dc824388551379b30cf
jwj <dnwls16071@naver.com>
2025-03-13 18:49:16 +0900

    [feat✨] module-batch - 멀티 모듈 컴포넌트 스캔 설정(#2)

--- module-batch/src/main/java/com/allimi/modulebatch/ModuleBatchApplication.java
+++ module-batch/src/main/java/com/allimi/modulebatch/ModuleBatchApplication.java
@@ -2,8 +2,19 @@ package com.allimi.modulebatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.allimi.modulecore.domain.news")
+@ComponentScan(basePackages = {
+	"com.allimi.modulebatch",
+	"com.allimi.modulecore",
+	"com.allimi.modulecore.config",
+	"com.allimi.moduleapi"
+})
 public class ModuleBatchApplication {
 
 	public static void main(String[] args) {
