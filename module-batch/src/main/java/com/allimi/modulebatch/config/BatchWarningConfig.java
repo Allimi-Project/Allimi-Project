4b4e60a72fa108515376570b272acfe0b5132f29
jwj <dnwls16071@naver.com>
2025-03-13 18:19:39 +0900

    [feat✨] module-batch - Spring Batch 관련 경고를 해결하기 위한 설정(#2)

---
+++ module-batch/src/main/java/com/allimi/modulebatch/config/BatchWarningConfig.java
@@ -0,0 +1,17 @@
+package com.allimi.modulebatch.config;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchWarningConfig {
+
+	// https://github.com/spring-projects/spring-batch/issues/4245
+	// https://github.com/spring-projects/spring-batch/issues/4519
+	@Bean
+	public static BeanDefinitionRegistryPostProcessor jobRegistryPostProcessorRemover() {
+		return registry -> registry.removeBeanDefinition("jobRegistryBeanPostProcessor");
+	}
+
+}
